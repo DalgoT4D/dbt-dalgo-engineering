9bb40265f73108a434e4d528b942cbaffcb0a2eb
Ishankoradia <ikoradia@umich.edu>
2026-02-22 19:15:07 +0530

    updates

--- tests/dummy_failing_test.sql
+++ tests/dummy_failing_test.sql
@@ -2,5 +2,4 @@
 -- It checks for a condition that should never be true
 
 SELECT 1 as failing_condition
-WHERE 1 = 2  -- This will never be true, so the test should pass
-   OR 'always' = 'fail'  -- This makes it fail by returning a row
+WHERE 1 = 1
